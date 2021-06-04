577aeb865fa8ab965b9a3e4f5d155c0e1b5721e5
Cori Lint <corilint@gmail.com>
2021-06-04 16:00:23 -0500

    commented out solutions

--- interviews.py
+++ interviews.py
@@ -11,82 +11,82 @@
 # Examples:
 # ['30th Feb 1997','1st Oct 2030','3rd Nov 1876','1st Dec 1865'] => ['1997-02-30','2030-10-01','1876-11-03','1865-12-01']
 
-def preprocessDate(dates):
-    # Write your code here
-    # split input on spaces => list of lists  [['00th', 'ABC', '0000'],['00th', 'ABC', '0000']]
-    # create a dictionary for months
-    # for each date in the dates list,
-        # edit date[0] by 
-            # slicing off last two char
-            # add a 0 if one digit
-        # edit month by changing to dict value
-        # don't edit year
+# def preprocessDate(dates):
+#     # Write your code here
+#     # split input on spaces => list of lists  [['00th', 'ABC', '0000'],['00th', 'ABC', '0000']]
+#     # create a dictionary for months
+#     # for each date in the dates list,
+#         # edit date[0] by 
+#             # slicing off last two char
+#             # add a 0 if one digit
+#         # edit month by changing to dict value
+#         # don't edit year
     
-    months = {"Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04", "May": "05", "Jun": "06", "Jul": "07", "Aug": "08", "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"}
-    formatted_dates = [] # creating a new array of formatted data, assuming we want to keep the input data in its original state. if the priority is to save space, I would instead replace each date in the original dates array.
+#     months = {"Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04", "May": "05", "Jun": "06", "Jul": "07", "Aug": "08", "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"}
+#     formatted_dates = [] # creating a new array of formatted data, assuming we want to keep the input data in its original state. if the priority is to save space, I would instead replace each date in the original dates array.
     
-    for i, date in enumerate(dates):
-        day, month, year = date.split()
-        day = day[0:-2]
-        if len(day) == 1:
-            day = f"0{day}"
-        month = months[month]
-        formatted_dates.append(f"{year}-{month}-{day}")
+#     for i, date in enumerate(dates):
+#         day, month, year = date.split()
+#         day = day[0:-2]
+#         if len(day) == 1:
+#             day = f"0{day}"
+#         month = months[month]
+#         formatted_dates.append(f"{year}-{month}-{day}")
             
-    return formatted_dates
+#     return formatted_dates
     
 
 ###** ARE THEY SPEEDING? **####
-def is_speeding(list_data):
+# def is_speeding(list_data):
     
-    # input at zero = first point
-    # input 1 = second point
+#     # input at zero = first point
+#     # input 1 = second point
     
-    # third item from second el of list minus
-    # take third item from first element of list
+#     # third item from second el of list minus
+#     # take third item from first element of list
     
-    # divide all by
-    # first item of second list minus
-    # first item of first list
+#     # divide all by
+#     # first item of second list minus
+#     # first item of first list
     
-    # return whether the result of above is over 100 - true/false
-    # + license plate
+#     # return whether the result of above is over 100 - true/false
+#     # + license plate
     
-#     while list_data:
+# #     while list_data:
     
-#         i_1, i_2 = list_data[0], list_data[1]
+# #         i_1, i_2 = list_data[0], list_data[1]
 
-#         time_1, time_2 = i_1[2], i_2[2]
-#         position_1, position_2 = i_1[0], i_2[0]
+# #         time_1, time_2 = i_1[2], i_2[2]
+# #         position_1, position_2 = i_1[0], i_2[0]
 
-#         speed = (position_2 - position_1) / ((time_2 - time_1)/3600)
+# #         speed = (position_2 - position_1) / ((time_2 - time_1)/3600)
 
-#         print([f'{i_1[1]}', speed > 100])
+# #         print([f'{i_1[1]}', speed > 100])
         
-#         list_data.pop(0)
-#         list_data.pop(0)
+# #         list_data.pop(0)
+# #         list_data.pop(0)
 
-# create dict
-# iterate through input
-# check whether license plate in dict
-# if not, add license plate as key
-# add value position, time
-# if already in dict
-# calculate speed & print boolean & license
+# # create dict
+# # iterate through input
+# # check whether license plate in dict
+# # if not, add license plate as key
+# # add value position, time
+# # if already in dict
+# # calculate speed & print boolean & license
 
-    license_lookup = {}
+#     license_lookup = {}
 
-    for speed_data in list_data:
-        p, l, t = speed_data
-        if l not in license_lookup:
-            license_lookup[l] = [p, t]
-        else:
-            # speed = (position_2 - position_1) / ((time_2 - time_1)/3600)
-            speed = (p - license_lookup[l][0]) / ((t - license_lookup[l][1])/3600)
-            print(l, speed > 100)
-        
+#     for speed_data in list_data:
+#         position, license, timestamp = speed_data
+#         if license not in license_lookup:
+#             license_lookup[license] = [position, timestamp]
+#         else:
+#             # speed = (position_2 - position_1) / ((time_2 - time_1)/3600)
+#             speed = (position - license_lookup[license][0]) / ((timestamp - license_lookup[license][1])/3600)
+#             print(license, speed > 100)
+#             # TODO: refactor for out-of-order        
 
-print(is_speeding([[1, "ABC-123", 1599066000], [2, "ABC-123", 1599066030], [1, "DEF-123",1599062378], [2, "DEF-123",1599062414], [1, "GHI-123",1599062435], [2, "GHI-123",1599062495], [1, "JKL-123",1599066000], [2, "JKL-123",1599066035]]))
+# # print(is_speeding([[1, "ABC-123", 1599066000], [2, "ABC-123", 1599066030], [1, "DEF-123",1599062378], [2, "DEF-123",1599062414], [1, "GHI-123",1599062435], [2, "GHI-123",1599062495], [1, "JKL-123",1599066000], [2, "JKL-123",1599066035]]))
 
 # print(is_speeding([[2, "JKL-123",1599066035], [2, "ABC-123",1599066030], [1, "ABC-123",1599066000], [2, "DEF-123",1599062414], [1, "GHI-123",1599062435], [2, "GHI-123",1599062495], [1, "JKL-123",1599066000], [1, "DEF-123",1599062378]]))
 
