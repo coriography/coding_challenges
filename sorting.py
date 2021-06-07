770662b5cb07b4d8edfe8295d7b115b942d817ea
Cori Lint <corilint@gmail.com>
2021-06-07 14:09:00 -0600

    added bubble sort and stubs for insertion and selection sort

--- sorting.py
+++ sorting.py
@@ -1,6 +1,7 @@
 def quick_sort(lst):
-    # quicksort practice - codewars
     # time complexity - worst case: O(n^2); average case: O(nlogn)
+    # general idea: choose a pivot and move items higher or lower then pivot.
+    # repeat with each half of the list and so on.
 
     # ** option 1: Hackbright solution ** #
 
@@ -35,6 +36,7 @@ def quick_sort(lst):
     # return lst
 
     # ** option 2: "buckets" solution ** #
+    # simpler solution, worse time & space complexity
 
     if len(lst) <= 1:
         return lst
@@ -51,9 +53,49 @@ def quick_sort(lst):
 
     return quick_sort(lo) + [pivot] + quick_sort(hi)
 
-print(quick_sort([3,6,1,3,2,8,4,8,3,8,3]))
-print(quick_sort([3,6,-1,3,2,-8,4,8,3,8,-3]))
-print(quick_sort([None]))
-print(quick_sort([2]))
-print(quick_sort([0]))
+# print(quick_sort([3,6,1,3,2,8,4,8,3,8,3]))
+# print(quick_sort([3,6,-1,3,2,-8,4,8,3,8,-3]))
+# print(quick_sort([None]))
+# print(quick_sort([2]))
+# print(quick_sort([0]))
     
+
+def bubble_sort(lst):
+
+    # time complexity - worst case: O(n^2); average case: O(nlogn)
+    # general idea: moving through list, compare two adjacent elements
+    # if 1st is greater than 2nd, swap them
+    # continue until list is sorted
+
+    if len(lst) <= 1:
+        return lst
+
+    for i in range(0, len(lst) - 1):
+        made_swap = False # keeps track of whether swap is made first time through
+        for j in range(0, len(lst) - 1 - i):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+                made_swap = True
+
+        if not made_swap: # nothing was swapped, so we can return
+            return lst
+
+    return lst
+
+# print(bubble_sort([3,6,-1,3,2,-8,4,8,3,8,-3]))
+# print(bubble_sort([3,6,1,3,2,8,4,8,3,8,3]))
+# print(bubble_sort([None]))
+# print(bubble_sort([2]))
+# print(bubble_sort([0]))
+
+def selection_sort(lst):
+    # time complexity - worst case: O(n^2); average case: O(n^2)
+    # general idea:
+
+    pass
+
+def insertion_sort(lst):
+    # time complexity - worst case: O(n^2); average case: O(n^2)
+    # general idea:
+
+    pass
