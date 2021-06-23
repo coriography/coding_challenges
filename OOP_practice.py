3f1c411c6d48e1247df40b5173dd3926ea1e29a0
Cori Lint <corilint@gmail.com>
2021-06-23 11:17:44 -0500

    completed exercise 2 - class without vars or methods

--- OOP_practice.py
+++ OOP_practice.py
@@ -1,7 +1,8 @@
 # OOP Exercise 1: Create a Vehicle class with max_speed and mileage instance attributes
 
 class Vehicle:
-    """A class for a Vehicle object.
+    """A class for a Vehicle object. 
+    #* To run doctests: $ python3 OOP_practice.py -v
 
     >>> c = Vehicle(40, 200000)
     >>> c.max_speed
@@ -16,7 +17,20 @@ class Vehicle:
         self.mileage = mileage
 
 
+# OOP Exercise 2: Create a Vehicle class without any variables and methods
 
+class VehicleSansMethods:
+    """a class for a Vehicle object - no variables or methods.
+    >>> c = VehicleSansMethods()
+    >>> c.max_speed
+    Traceback (most recent call last):
+    AttributeError: 'VehicleSansMethods' object has no attribute 'max_speed'
+    >>> c.mileage
+    Traceback (most recent call last):
+    AttributeError: 'VehicleSansMethods' object has no attribute 'mileage'
+    """
+
+    pass
 
 
 if __name__ == "__main__":
