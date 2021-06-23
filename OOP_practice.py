da2d112338405311f1e62a160df4f4617ba2d534
Cori Lint <corilint@gmail.com>
2021-06-23 11:36:05 -0500

    completed exercise 3 by adding Bus child class

--- OOP_practice.py
+++ OOP_practice.py
@@ -33,6 +33,32 @@ class VehicleSansMethods:
     pass
 
 
+# OOP Exercise 3: Create a child class Bus that will inherit all of the variables and methods of the Vehicle class
+# expected output: "Vehicle Name: School Volvo Speed: 180 Mileage: 12"
+
+# given:
+class ParentVehicle:
+    """given class for a Parent Vehicle."""
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+class Bus(ParentVehicle):
+    """Class for a Bus object, inheriting from ParentVehicle class.
+    >>> b = Bus("School Volvo", 180, 12)
+    >>> b.show()
+    Vehicle Name: School Volvo Speed: 180 Mileage: 12
+    """
+
+    def __init__(self, name, max_speed, mileage):
+        super().__init__(name, max_speed, mileage)
+
+    def show(self):
+        print("Vehicle Name:", self.name, "Speed:", self.max_speed, "Mileage:", self.mileage)
+
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
