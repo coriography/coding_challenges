b9fb0c792f1d9111845fd201d0269e6abfa0eb85
Cori Lint <corilint@gmail.com>
2021-07-01 17:21:42 -0500

    added restock warehouse problem and solution

--- interviews.py
+++ interviews.py
@@ -1,4 +1,60 @@
 
+
+###** Restocking the Warehouse **###
+
+
+# A purchasing manager must buy a specific number of units of an item to replenish the warehouse. The primary supplier has a list of containers, each with a number of units. The manager must buy contiguous containers, starting at container 0 and continuing until at least the desired number has been purchased. If there are not enough units available, they must be purchased from another supplier. If any excess items must be purchased, they must be resold. Determine the remaining number of items to be purchased or sold after purchasing from the primary supplier.
+
+# Example
+# n=5
+# itemCount = [10, 20, 30, 40, 15]
+# target = 80
+
+# The manager starts buying at index 0 and continues until all available units are purchased or until at least 80 units have been purchased. The manager will buy containers with itemCounts = 10 + 20 + 30 + 40 = 100. Since too many items were purchased, the number sold is purchased - target = 100 - 80 = 20 units.
+
+# If the target = 130, the manager will purchase all of the units from the primary supplier for a total of purchases = 115.  Then another target - purchases = 130 - 115 = 15 additional units must be purchased.
+
+# Function Description
+
+# Complete the function restock in the editor below.
+
+# restock has the following parameter(s):
+#     int itemCount[n]:  an array of integers that denote the item counts of the each container in the order they must be purchased
+#    int : target: an integer that denotes the target number of items needed
+# Returns:
+#     int:  number of units that must be resold or that must be purchased from an alternate supplier.
+
+# Constraints
+
+# 1 ≤ n ≤ 105
+# 1 ≤ target ≤ 109
+# 1 ≤ itemCount[i] ≤ 109
+#
+# Complete the 'restock' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY itemCount
+#  2. INTEGER target
+#
+
+def restock(item_count, target):
+    # iterate through itemCount and add 
+    # stop when current count is >= target OR end of itemCount
+    # compare current count to target and return difference
+    
+    current_count = 0
+    
+    for container_count in item_count:
+        if current_count < target:
+            current_count += container_count
+        else:
+            break
+    
+    return abs(current_count - target)
+
+
+
 ###** Points Covering Segments **###
 
 # You are given an array of segments (pairs that represent the lower and upper bounds of a segment, inclusive). Determine the minimum number of points that can be placed somewhere on the number line, so that each segment contains at least one point.
