983e8b78b3fcfb0a4cca124341d57dc9b13b0f91
Cori Lint <corilint@gmail.com>
2021-07-16 12:43:10 -0500

    added num of steps to zero problem and solution

--- general.py
+++ general.py
@@ -1,4 +1,22 @@
 
+###** number of steps to zero **###
+# https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+# Given a non-negative integer num, return the number of steps to reduce it to zero. If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        
+        num_steps = 0
+        
+        while num > 0:
+            num_steps += 1
+            if num % 2 == 0:
+                num = num/2
+            else:
+                num = num - 1
+                
+        return num_steps
+
 ###### ** ABBREVIATE WORDS ** ######
 
 def abbreviate(s):
