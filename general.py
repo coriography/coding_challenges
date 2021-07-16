cbe87976475ae11ce548b6e982da47f4e384c27d
Cori Lint <corilint@gmail.com>
2021-07-16 13:38:40 -0500

    added 'add zeroes' problem and solution

--- general.py
+++ general.py
@@ -1,4 +1,58 @@
 
+###** move zeroes **###
+
+# https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/567/
+
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+# Note that you must do this in-place without making a copy of the array.
+
+ 
+
+# Example 1:
+
+# Input: nums = [0,1,0,3,12]
+# Output: [1,3,12,0,0]
+# Example 2:
+
+# Input: nums = [0]
+# Output: [0]
+
+# class Solution:
+#     def moveZeroes(self, nums: List[int]) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#         i, j = 0, 0
+        
+#         # [0,1]
+#         #  i 
+#         # [0]
+        
+#         # i deals with incrementing
+#         # j deals with zeroes
+        
+#         # for as many times as length of nums,
+#             # if nums[i] is zero, 
+#                 # pop it off and add to new array # [0]
+#                 # don't increment i
+#             # else if nums[i] not zero,
+#                 # increment i
+                
+#         # extend nums with new array
+        
+#         # if len(nums) <= 1:
+#         #     return nums
+            
+#         while i < len(nums):
+#             i += 1
+#             if nums[j] == 0:
+#                 nums.append(nums.pop(j))
+#             else:
+#                 j += 1
+                
+#         return nums
+
 ###** Buy and sell stocks II **###
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
 
