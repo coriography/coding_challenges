5c152fa15a37ee95171d6537a4e35f9c58d120e0
Cori Lint <corilint@gmail.com>
2021-07-19 18:17:15 -0500

    created RecruitingTool class with stubs for all methods

---
+++ yaakov_challenge/yaakov_challenge.py
@@ -0,0 +1,39 @@
+class RecruitingTool:
+
+    def __init__(self) -> None:
+        pass
+        # set all attributes to 0 or None
+
+    def is_senior_role():
+        pass
+        # returns Boolean - contains "senior"
+
+    def is_data_role():
+        pass
+        # returns Boolean - contains "data," "analyst," or "analytics"
+
+    def is_leadership_role():
+        pass
+        # returns Boolean - contains "manager"
+
+    def get_years_experience():
+        pass
+        # get years in each job (need to pass to average function later)
+        # total years
+        # years in senior role
+        # years in data role
+        # years in leadership
+
+    def get_current_job():
+        pass
+        # get job with end date "null"
+        # get title
+        # get length in position
+
+    def get_average_years_in_job():
+        pass
+        # take years in each job from get_years_experience
+
+    def print_candidate_info():
+        pass
+        # print candidate info
